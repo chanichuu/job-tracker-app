1102e404a29d77e2024a59ec018b0aed5d9390bb
Chani <c.m.huttenloher@gmail.com>
2025-05-07 23:03:27 +0900

    chore: register Job, Address, Contact models in admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Job, Address, Contact
 
 # Register your models here.
+
+admin.site.register(Job)
+admin.site.register(Address)
+admin.site.register(Contact)
